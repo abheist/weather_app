e89ab07b2ea0d51a4e7c266eb1bab268eef16c53
Abhishek <abhishek291093@gmail.com>
2018-01-31 01:33:29 +0530

    AKS | install openweather api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 class App extends Component {
+    api_key = "ea969fca13f2aa3cb2d2be176bec518f";
     render() {
         return <div className="App">Hello world!</div>;
     }
