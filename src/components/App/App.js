ca92b98b53071a4a40c941eb6d38517955516d4c
Abhishek <abhishek291093@gmail.com>
2018-01-31 01:43:58 +0530

    AKS | api for weather data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 class App extends Component {
-    api_key = "ea969fca13f2aa3cb2d2be176bec518f";
     render() {
         return <div className="App">Hello world!</div>;
     }
