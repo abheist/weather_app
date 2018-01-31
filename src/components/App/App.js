b3999c3b047db297a27e7e56ce155d32510a9db6
Abhishek <abhishek291093@gmail.com>
2018-01-31 23:08:46 +0530

    AKS | outer layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import css from "./App.css";
 
 class App extends Component {
     render() {
-        return <div className="App">Hello world!</div>;
+        return (
+            <div className="App">
+                <div className="header">
+                    <h1>WEATHER APP</h1>
+                </div>
+                <div className="container">
+                    <div className="container-card">weather card</div>
+                </div>
+            </div>
+        );
     }
 }
 
