b7c0d40f962a22a339250e7f1143e7d8fd19e2ad
Abhishek <abhishek291093@gmail.com>
2018-02-04 15:18:43 +0530

    AKS | created another component for frequest weathers

---
+++ src/components/frequestWeather.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+class FrequestWeatherConstants extends React.Component {
+    render() {
+        return (
+            <div className="frequent-box current-temperature-box flex-row align-center">
+                <div className="flex-column align-center">
+                    <div
+                        style={{
+                            height: 100,
+                            width: 100,
+                            backgroundImage: `url(${thermometer})`,
+                            backgroundSize: "contain",
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat"
+                        }}
+                    />
+                    <p className="margin-top-20 font-20">Temp</p>
+                </div>
+                <div
+                    style={{
+                        marginLeft: 30
+                    }}
+                >
+                    <div>
+                        <p className="font-90 weight-500">23°</p>
+                        <p className="font-10 margin-top--10 weight-200">
+                            Celsius
+                        </p>
+                    </div>
+                    <div>
+                        <p className="font-60 weight-500">25°</p>
+                        <p className="font-14 margin-top--6 weight-200">
+                            Feels like
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FrequestWeatherConstants;
